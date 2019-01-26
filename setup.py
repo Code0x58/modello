c62dec1060e7724a1415f59ac711881a9fc9ec02
Oliver Bristow <obristow@mintel.com>
2019-01-26 13:07:40 +0000

    Fix tests

--- setup.py
+++ setup.py
@@ -9,7 +9,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-TEST_REQUIRES = ["pytest>=3.9", "pytest-cov", "pytest-mypy", "pytest-flake8", "flake8-docstrings", "flake8-isort"]
+TEST_REQUIRES = [
+    "flake8-docstrings",
+    "flake8-isort",
+    "pytest-cov>=2.6.1",
+    "pytest-flake8",
+    "pytest-mypy",
+    "pytest>=4.1",
+]
 setup(
     author="Oliver Bristow",
     author_email="github+pypi@oliverbristow.co.uk",
