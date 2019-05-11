9244969392ad4683ee8503209d5d745cad15190a
Oliver Bristow <obristow@mintel.com>
2019-05-11 20:04:56 +0100

    Add 2019 tax rates

--- examples/jobs.py
+++ examples/jobs.py
@@ -4,18 +4,22 @@ from functools import lru_cache
 from modello import InstanceDummy, Modello
 from sympy import Eq, Function, Heaviside, Piecewise, Rational, S, Symbol, oo, solve
 
+YEAR = 2019
 
-def generate_piecewise_income_tax(year=2019):
+
+def generate_piecewise_income_tax(year=YEAR):
     """Assuming band A."""
     # 2019-2020 UK tax brackets
+    # XXX: this omits the income limit for personal allowance. A fix for this could be to add an additional
+    #  bracket from £100,000 to £125,000 (for 2019)
     year_band_brackets = {
         2018: ((-oo, 0), (11850, Rational(1, 5)), (46350, Rational(2, 5)), (150000, Rational(9, 20))),
-        2019: ((-oo, 0), (1250, Rational(1, 5)), (37500, Rational(2, 5)), (150000, Rational(9, 20))),
+        2019: ((-oo, 0), (12500, Rational(1, 5)), (37500, Rational(2, 5)), (150000, Rational(9, 20))),
     }
     return generate_piecewise_function(year_band_brackets[year])
 
 
-def generate_piecewise_national_insurance(year=2018, band="A"):
+def generate_piecewise_national_insurance(year=YEAR, band="A"):
     """Assuming band A."""
     # 2018-2019 UK band A NI
     year_band_brackets = {
@@ -27,12 +31,21 @@ def generate_piecewise_national_insurance(year=2018, band="A"):
             "J": ((-oo, 0), (702 * 12, Rational(2, 100))),
             "M": ((-oo, 0), (702 * 12, Rational(12, 100)), (3863 * 12, Rational(2, 100))),
             "Z": ((-oo, 0), (702 * 12, Rational(2, 100))),
+        },
+        2019: {
+            "A": ((-oo, 0), (719 * 12, Rational(12, 100)), (4167 * 12, Rational(2, 100))),
+            "B": ((-oo, 0), (219 * 12, Rational(585, 100)), (4167 * 12, Rational(2, 100))),
+            "C": ((-oo, 0),),
+            "H": ((-oo, 0), (719 * 12, Rational(12, 100)), (4167 * 12, Rational(2, 100))),
+            "J": ((-oo, 0), (719 * 12, Rational(2, 100))),
+            "M": ((-oo, 0), (719 * 12, Rational(12, 100)), (4167 * 12, Rational(2, 100))),
+            "Z": ((-oo, 0), (719 * 12, Rational(2, 100))),
         }
     }
     return generate_piecewise_function(year_band_brackets[year][band])
 
 
-def generate_piecewise_employer_national_insurance(year=2018, band="A"):
+def generate_piecewise_employer_national_insurance(year=YEAR, band="A"):
     """Assuming band A."""
     # 2018-2019 UK band A NI
     year_band_brackets = {
@@ -44,6 +57,15 @@ def generate_piecewise_employer_national_insurance(year=2018, band="A"):
             "J": ((-oo, 0), (702 * 12, Rational(138, 1000))),
             "M": ((-oo, 0), (3863 * 12, Rational(138, 1000))),
             "Z": ((-oo, 0), (3863 * 12, Rational(138, 1000))),
+        },
+        2019: {
+            "A": ((-oo, 0), (719 * 12, Rational(138, 1000))),
+            "B": ((-oo, 0), (719 * 12, Rational(138, 1000))),
+            "C": ((-oo, 0), (719 * 12, Rational(138, 1000))),
+            "H": ((-oo, 0), (4167 * 12, Rational(138, 1000))),
+            "J": ((-oo, 0), (719 * 12, Rational(138, 1000))),
+            "M": ((-oo, 0), (4167 * 12, Rational(138, 1000))),
+            "Z": ((-oo, 0), (4167 * 12, Rational(138, 1000))),
         }
     }
     return generate_piecewise_function(year_band_brackets[year][band])
@@ -98,9 +120,9 @@ class Job(Modello):
 
     >>> job = Job("job", salary=32000, hours=253*8, expenses=0)
     >>> job.hourly_income.evalf(4)
-    11.37
+    12.50
     >>> job.employer_expense.evalf(7)
-    35253.49
+    35225.34
     """
 
     salary = InstanceDummy("salery", rational=True, positive=True)
